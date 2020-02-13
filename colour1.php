584e6623f1bb5a4864926809e9840a46a5badebe
Madhura-Khaire <59690231+Madhura-Khaire@users.noreply.github.com>
2020-02-13 16:35:08 +0530

    Add files via upload

---
+++ colour1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$color = "red";
+$color1 = "blue";
+$color2 = "yellow";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $color1 . "<br>";
+echo "My boat is " . $color2 . "<br>";
+?>
+
+</body>
+</html>
